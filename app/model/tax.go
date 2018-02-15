aab4212ee80a1588d58a6c6e8f073e8cdb769a33
somrod <it@nopadol.com>
2018-02-15 09:08:21 +0700

    Edit Tax

--- app/model/tax.go
+++ app/model/tax.go
@@ -1,6 +1,6 @@
 package model
 
 func SelectDataTax() error {
-	sql := `select ifnull(sum(total_amount),0) as total_amount from tax_temp where doc_date = ?`
+	//sql := `select ifnull(sum(total_amount),0) as total_amount from tax_temp where doc_date = ?`
 	return nil
 }
